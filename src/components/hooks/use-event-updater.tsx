96cebdb823b5eb0b91db5e72eb206ce768f8efb4
Matt Newbigging <mrnewbigging@gmail.com>
2025-10-26 12:47:39 +0000

    setup

--- src/components/hooks/use-event-updater.tsx
+++ src/components/hooks/use-event-updater.tsx
@@ -1,17 +1,17 @@
 import { useEffect, useReducer } from "react";
-import { eventListener, EventMap } from "../../events/event-listener";
+import { eventUpdater, GameEvent } from "../../events/event-updater";
 
-export function useEventUpdater<E extends keyof EventMap>(...events: E[]) {
+export function useEventUpdater(...events: GameEvent[]) {
   const [, forceUpdate] = useReducer((x) => x + 1, 0);
 
   useEffect(() => {
     // On first mount, subscribe to the event
-    events.forEach((event) => eventListener.on(event, forceUpdate));
+    events.forEach((event) => eventUpdater.on(event, forceUpdate));
 
     // On unmount
     return () => {
       // Unsubscribe from the event
-      events.forEach((event) => eventListener.off(event, forceUpdate));
+      events.forEach((event) => eventUpdater.off(event, forceUpdate));
     };
   }, [...events]);
 }
---
+++ src/events/event-updater.ts
@@ -0,0 +1,34 @@
+export type GameEvent = "game-loaded" | "game-started";
+
+type EventCallback = () => void;
+
+class EventUpdater {
+  private callbacks = new Map<GameEvent, Set<EventCallback>>();
+
+  on(event: GameEvent, callback: EventCallback) {
+    const callbacks = this.callbacks.get(event) ?? new Set<EventCallback>();
+    callbacks.add(callback);
+    this.callbacks.set(event, callbacks);
+  }
+
+  off(event: GameEvent, callback: EventCallback) {
+    const callbacks = this.callbacks.get(event);
+    if (!callbacks) return;
+
+    callbacks.delete(callback);
+
+    if (!callbacks.size) {
+      this.callbacks.delete(event);
+    } else {
+      this.callbacks.set(event, callbacks);
+    }
+  }
+
+  fire(event: GameEvent) {
+    const callbacks = this.callbacks.get(event);
+    if (!callbacks) return;
+    callbacks.forEach((cb) => cb());
+  }
+}
+
+export const eventUpdater = new EventUpdater();
