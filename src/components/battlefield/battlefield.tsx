566437813a8acc19ab1b95d40391a30cb7e07f79
Matt Newbigging <mrnewbigging@gmail.com>
2025-10-26 13:15:39 +0000

    battlefield layout

--- src/components/battlefield/battlefield.tsx
+++ src/components/battlefield/battlefield.tsx
@@ -1,5 +1,36 @@
+import { EnemyCard } from "../enemy-card/enemy-card";
 import "./battlefield.scss";
 
 export function Battlefield() {
-  return <div className="battlefield"></div>;
+  return (
+    <div className="battlefield">
+      <BattlefieldColumn />
+      <BattlefieldColumn />
+      <BattlefieldColumn />
+      <BattlefieldColumn />
+      <BattlefieldColumn />
+    </div>
+  );
+}
+
+function BattlefieldColumn() {
+  return (
+    <div className="column">
+      <div className="enemy-card-area">
+        <EnemyCard />
+      </div>
+      <div className="enemy-card-area">
+        <EnemyCard />
+      </div>
+      <div className="enemy-card-area">
+        <EnemyCard />
+      </div>
+      <div className="enemy-card-area">
+        <EnemyCard />
+      </div>
+      <div className="enemy-card-area">
+        <EnemyCard />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/enemy-card/enemy-card.tsx
@@ -0,0 +1,5 @@
+import "./enemy-card.scss";
+
+export function EnemyCard() {
+  return <div className="enemy-card"></div>;
+}
